5e955175b55814bbe4bde1330d7528fb3a5de57b
WAHYU SAHRI RHAMADHAN_1F <91419957+sahrt@users.noreply.github.com>
2021-12-15 11:59:08 +0700

    Add files via upload

---
+++ PEMROGRAMAN PAKDIM PRAKTIKUM/src/com/tutorial/praktikum1_if.java
@@ -0,0 +1,30 @@
+package com.tutorial;
+
+import java.util.Scanner;
+
+public class praktikum1_if {
+    public static void main(String[] args) {
+        //soal 1
+        Scanner inputSahri = new Scanner(System.in);
+        int a,b,i;
+        System.out.println("Nilai Awal = ");
+        a= inputSahri.nextInt();
+        System.out.println("Nilai Akhir = ");
+        b= inputSahri.nextInt();
+        i=0;
+        //masuk kepemrogramnya
+        // pengunaan dari if
+        if(a%2==0){
+            while (i<=a)
+                i=i
+            System.out.println(a+"");
+            a++;
+
+            }else if(b%2==0){
+            System.out.println();
+        }
+    }
+
+
+
+}
